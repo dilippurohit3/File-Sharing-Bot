c68699fc14865fce51784a6c7c225033edb216dd
dilip purohit <91429364+dilippurohit3@users.noreply.github.com>
2023-11-01 00:16:41 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id=730905314'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='tg://user?id=730905314'>Click here To Buy</a>\n</b>",
+            text = f"<b>○ Creator : <a href='https://t.me/OpclaimerakaLinX'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/OpclaimerakaLinX'>Click here To Buy</a>\n</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
